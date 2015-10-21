e8784251ac787b5ca6483073bb1ecd662189e5a7
unknown <MYSKILLS@MYSKILLS-NB04.myskills.com>
2015-10-21 15:58:14 +0800

    added sample blog

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -52,3 +52,12 @@ Route::post('login-submit', function() {
 	$result .= "| Password:". Request::get('password');
 	return $result;
 });
+
+route::get('posts', function() {
+	return view('posts.index');
+});
+
+Route::get('posts/{id}', function($id) {
+	$post = App\Post::find($id);
+	return view('posts.show')->with('post',$post);
+});
